06c4bd7f7a89db9fa338df505b23bad6c519cc62
andre_zktt <andrezicatti@gmail.com>
2025-09-03 13:15:43 -0300

    :card_file_box: database: create a lifespan function to establish a database connection as soon as the backend starts

--- app/app.py
+++ app/app.py
@@ -1,10 +1,26 @@
 from fastapi import FastAPI
+from contextlib import asynccontextmanager
 from core.config import settings
 from beanie import init_beanie
 from motor.motor_asyncio import AsyncIOMotorClient
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    print('🔹 Iniciando conexão com o bando de dados...')
+    client_db = AsyncIOMotorClient(settings.MONGO_CONNECTION_STRING).todoapp
+    await init_beanie(
+        database=client_db,
+        document_models=[
+            
+        ]
+    )
+    print('✅ Conexão estabelecida com sucesso.')
+    yield
+    print('🛑 Encerrando conexão com o banco de dados...')
+    client_db.close()
 
-@app.get('/')
-async def hello_world():
-    return 'Hello, world!'
+app = FastAPI(
+    lifespan=lifespan, 
+    title=settings.PROJECT_NAME, 
+    openapi_url=f'{settings.API_V1_STR}/openapi.json'
+)
--- app/core/config.py
+++ app/core/config.py
@@ -5,12 +5,12 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     PROJECT_NAME: str = 'TODOFast'
-    API_V1_STR = '/api/v1'
+    API_V1_STR: str = '/api/v1'
     
-    JWT_SECRET_KEY = config('JWT_SECRET_KEY', cast=str)
-    JWT_REFRES_SECRET_KEY = config('JWT_REFRESH_SECRET_KEY', cast=str)
+    JWT_SECRET_KEY: str = config('JWT_SECRET_KEY', cast=str)
+    JWT_REFRESH_SECRET_KEY: str = config('JWT_REFRESH_SECRET_KEY', cast=str)
     
-    ALGORITHM = 'HS256'
+    ALGORITHM: str = 'HS256'
     
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60
     REFRESH_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 7
@@ -18,7 +18,7 @@ class Settings(BaseSettings):
     BACKEND_CORS_ORIGINS: List[AnyHttpUrl] = []
 
     # Database Connection
-    MONGO_CONNECTION_STRING = config('MONGO_CONNECTION_STRING', cast=str)
+    MONGO_CONNECTION_STRING: str = config('MONGO_CONNECTION_STRING', cast=str)
     
     class Config:
         case_sensitive = True
