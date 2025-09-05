aca7445662f8e3f6370cddd3d09b3ea5cc8ba7a1
andre_zktt <andrezicatti@gmail.com>
2025-09-05 16:37:37 -0300

    :lock: security: implement refresh token method

--- app/api/auth/jwt.py
+++ app/api/auth/jwt.py
@@ -1,13 +1,16 @@
-from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi import APIRouter, Depends, HTTPException, status, Body
 from fastapi.security import OAuth2PasswordRequestForm
 from typing import Any
+from jose import jwt, JWTError, ExpiredSignatureError
+from pydantic import ValidationError
 
-from services.user_service import UserService
-from core.security import create_access_token, create_refresh_token
-from schemas.auth_schema import TokenSchema
-from schemas.user_schema import UserDetail
-from models.user_model import User
 from api.dependencies.user_deps import get_current_user
+from core.config import settings
+from core.security import create_access_token, create_refresh_token
+from models.user_model import User
+from schemas.auth_schema import TokenSchema, TokenPayload
+from schemas.user_schema import UserDetail
+from services.user_service import UserService
 
 auth_router = APIRouter()
 
@@ -30,3 +33,41 @@ async def login(data: OAuth2PasswordRequestForm = Depends()) -> Any:
 @auth_router.post('/test-token', summary='Testando o Token', response_model=UserDetail)
 async def test_token(user: User = Depends(get_current_user)):
     return user
+
+@auth_router.post('/refresh', summary='Refresh Token', response_model=TokenSchema)
+async def refresh_token(refresh_token: str = Body(...)):
+    try:
+        payload = jwt.decode(
+            token=refresh_token,
+            key=settings.JWT_REFRESH_SECRET_KEY,
+            algorithms=[settings.ALGORITHM]
+        )
+        token_data = TokenPayload(**payload)
+
+    except ExpiredSignatureError:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail='Token Expirado.',
+            headers={'WWW-Authenticate': 'Bearer'}
+        )
+
+    except(JWTError, ValidationError):
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail='Erro na validação do Token.',
+            headers={'WWW-Authenticate': 'Bearer'}
+        )
+
+    user = await UserService.get_user_by_id(id=token_data.sub)
+
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail='Não foi possível encontrar o usuário.',
+            headers={'WWW-Authenticate': 'Bearer'}
+        )
+
+    return {
+        'access_token': create_access_token(user.user_id),
+        'refresh_token': create_refresh_token(user.user_id)
+    }
--- app/api/handlers/user.py
+++ app/api/handlers/user.py
@@ -1,7 +1,10 @@
-from fastapi import APIRouter, HTTPException, status
+from fastapi import APIRouter, HTTPException, status, Depends
+import pymongo
+
 from schemas.user_schema import UserAuth, UserDetail
 from services.user_service import UserService
-import pymongo
+from api.dependencies.user_deps import get_current_user
+from models.user_model import User
 
 user_router = APIRouter()
 
@@ -14,3 +17,7 @@ async def create_user(data: UserAuth):
             status_code=status.HTTP_400_BAD_REQUEST,
             detail='O username ou email deste usuário já existe.'
         )
+
+@user_router.get('/me', summary='Detalhes do usuário logado.', response_model=UserDetail)
+async def get_me(user: User = Depends(get_current_user)):
+    return user
