fd52142cfbe32336b9bdc42e39bdbb0bc52d7a87
andre_zktt <andrezicatti@gmail.com>
2025-09-08 17:11:03 -0300

    :recycle: refactor: re-added routing system

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,13 @@
+import { Route, BrowserRouter as Router, Routes} from 'react-router-dom'
 
 function App() {
   return (
-    <div>
-      Hello, world!
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/login' element={<h1>Login</h1>}></Route>
+        <Route path='/register' element={<h1>Cadastro</h1>}></Route>
+      </Routes>
+    </Router>
   );
 }
 
